988ba4187dd817cb73cd07f584268f276d15b97c
Dustin Kelley <kelleydustin2018@gmail.com>
2023-01-05 10:46:19 -0600

    added sign out functionality

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,94 +1,101 @@
-import {React, useState} from 'react'
+import {React, useState, useContext} from 'react'
 import { 
-  createUserDocumentFromAuth,
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword
  } from '../../utils/firebase/firebase.utils';
+
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { UserContext } from '../../contexts/user.context';
 
 import './sign-in-form.styles.scss';
+
+
 const defaultFormFields = {
-    email: '',
-    password: ''
-}
+  email: '',
+  password: '',
+};
 
 const SignInForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const { email, password } = formFields;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
 
-    console.log(formFields);
+  const { setCurrentUser } = useContext(UserContext);
 
-    const resetFormFields = () => {
-        setFormFields(defaultFormFields);
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    setCurrentUser(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      resetFormFields();
+      setCurrentUser(user);
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(error);
+      }
     }
+  };
 
-    const signInWithGoogle = async () => {
-      const { user } = await signInWithGooglePopup();
-      await createUserDocumentFromAuth(user);
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-          const response = await signInAuthUserWithEmailAndPassword(email, password);
-          console.log(response);
-          resetFormFields();
-        } catch (error) {
-
-            switch(error.code){
-              case 'auth/wrong-password':
-                alert('Incorrect Password for Email')
-                break
-              case 'auth/user-not-found':
-                alert('No User Associated With This Email')
-                break;
-              default:
-                console.log(error);
-              
-            }
-          };
-    };
-   
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-
-        setFormFields({...formFields, [name]: value});
-    };
+    setFormFields({ ...formFields, [name]: value });
+  };
 
   return (
-    <div className='sign-up-container'>
-    <h2>Already have an account?</h2>
-    <span>Sign in with your email and password</span>
-        <form onSubmit={handleSubmit}>
+    <div className='sign-in-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
 
-            <FormInput 
-            label='Email' 
-            type='email' 
-            required 
-            onChange={handleChange} 
-            name='email' 
-            value={email}
-            />
-
-            <FormInput 
-            label='Password' 
-            type='password' 
-            required 
-            onChange={handleChange} 
-            name='password' 
-            value={password}
-            />
-          <div className='buttons-container'>
-            <Button type='submit'>Sign in</Button>
-            <Button type='button' buttonType='google' onClick={signInWithGoogle} >Google Sign In</Button>
-          </div>
-           
-        </form>
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+          <Button
+            buttonType='google'
+            type='button'
+            onClick={signInWithGoogle}
+          >
+            Sign In With Google
+          </Button>
+        </div>
+      </form>
     </div>
-  )
-   };
+  );
+};
 
 export default SignInForm;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './contexts/user.context';
+import './index.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
+    <BrowserRouter>
+      <UserProvider>
         <App />
-  </BrowserRouter>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
